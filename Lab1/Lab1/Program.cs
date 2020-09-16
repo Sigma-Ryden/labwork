50ba21c86c9f4e1c2f35dd5902d4f6c3306a00f6
Sigma-Ryden <71389444+Sigma-Ryden@users.noreply.github.com>
2020-09-16 23:00:40 +0300

    Add files via upload

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter value by Om for the transistor R1: ");
+            double R1 = 0;
+            string str = Console.ReadLine();
+            R1 = Convert.ToDouble(str);
+
+            Console.Write("Enter value by Om for the transistor R2: ");
+            double R2 = 0;
+            str = Console.ReadLine();
+            R2 = Convert.ToDouble(str);
+
+            Console.Write("Resistance of R1 and R2 by Om = ");
+            Console.WriteLine(R1 * R2 / (R1 + R2));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value for nk");
+            Console.WriteLine("where nk is natural number");
+
+            int nk = 0;
+
+            do
+            {
+                Console.Write("nk: ");
+
+                string str = Console.ReadLine();
+                nk = Convert.ToInt32(str);
+
+                if (nk < 0)
+                {
+                    Console.WriteLine("Put the correct value!");
+                }
+
+            } while (nk < 0);
+
+            Console.WriteLine("Enter the value for nn");
+            Console.WriteLine("where nn is natural number and nn => nk");
+
+            int nn = 0;
+
+            do
+            {
+                Console.Write("nn: ");
+
+                string str = Console.ReadLine();
+                nn = Convert.ToInt32(str);
+
+                if (nn < nk)
+                {
+                    Console.WriteLine("Put the correct value!");
+                }
+
+            } while (nn < nk);
+
+            double sum = 0;
+
+            for (int k = nk; k <= nn; k++)
+            {
+                sum += (k * k - 1) / (Math.Pow(-1, k + 1) * k * k + 7);
+            }
+
+            Console.Write($"Sum of the series from {nk} to {nn} = {sum}");
+            Console.ReadLine();
+        }
+    }
+}
