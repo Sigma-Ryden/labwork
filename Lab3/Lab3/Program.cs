85534de8e9bf8e3b656dab3262dfcbb97e4e9c96
Sigma-Ryden <71389444+Sigma-Ryden@users.noreply.github.com>
2020-09-21 22:56:13 +0300

    Add files via upload

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter any natural number: ");
+
+            int a = 0;
+            string str = Console.ReadLine();
+            a = Convert.ToInt32(str);
+
+            if (a > 9 && a < 100 && a % 2 == 0)
+            {
+                Console.WriteLine(true);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
