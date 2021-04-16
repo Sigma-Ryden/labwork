d71c7ebcf4ca1968e8a592eaea373ccdb8eb80b9
Sigma-Ryden <71389444+Sigma-Ryden@users.noreply.github.com>
2021-04-16 09:34:31 +0300

    Add files via upload

---
+++ Lab12/Entities/DivideOperation.cs
@@ -0,0 +1,9 @@
+﻿using Lab_12.Interfaces;
+
+namespace Lab_12.Entities
+{
+    public class DivideOperation : IOperation
+    {
+        public double Operate(double firstOperand, double secondOperand) => firstOperand / secondOperand;
+    }
+}
