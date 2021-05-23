ec52d6282e92ce642bc5372b89fa9ce0a836a6da
Sigma-Ryden <71389444+Sigma-Ryden@users.noreply.github.com>
2021-05-23 23:31:52 +0300

    Add files via upload

---
+++ Lab15(4)/Lab 4/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Lab_4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement el in MainGrid.Children)
+                if (el is Button) ((Button)el).Click += Button_Click;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "AC") label.Text = "";
+            else if(str == "=")
+            {
+                string result = new DataTable().Compute(label.Text,null).ToString();
+                label.Text = result;
+            }
+            else label.Text += str;
+        }
+    }
+}
