a02f4af76666068abe68ca942b77c86cf02cbe81
Sigma-Ryden <71389444+Sigma-Ryden@users.noreply.github.com>
2021-05-23 23:33:22 +0300

    Add files via upload

---
+++ Lab18(7)/Lab_7/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace Lab_7
+{
+    public partial class MainWindow : Window
+    {
+        static string link = @"C:\Dir";
+        public MainWindow()
+        {
+            InitializeComponent();
+            MessageBox.Show($"Your current path: {link}");
+        }
+        static void CreateDirRecursive(int i = 0)
+        {
+            if (i < 100)
+            {
+                var directory = new DirectoryInfo(link);
+                directory.CreateSubdirectory($"Folder_{i}");
+                link += @$"\Folder_{i}";
+                CreateDirRecursive(i + 1);
+            }
+            else return;
+        }
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            CreateDirRecursive();
+            MessageBox.Show($"Max of created folders 100!");
+            link = @"C:\Dir";
+        }
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            var directory_clear = new DirectoryInfo(link);
+            directory_clear.Delete(true);
+            MessageBox.Show($"Dir has been deleted!");
+        }
+    }
+}
