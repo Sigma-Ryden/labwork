6cf646aa7977ef54e562a019cb0ebdd182deef12
Sigma-Ryden <71389444+Sigma-Ryden@users.noreply.github.com>
2021-06-21 18:04:49 +0300

    Add files via upload

---
+++ Lab19(8)/Lab8/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab_8
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ Lab19(8)/Lab8/MessageHandler.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab_8
+{
+    public delegate void MessageHandler(Message message);
+}
